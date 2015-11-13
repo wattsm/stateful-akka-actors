1717d936e1a27fa5c37b0172a313b8e9585b29a5
Mark Watts <mark.watts@hotmail.co.uk>
2015-11-13 07:15:45 +0000

    Add source code

---
+++ CSharp/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+
+namespace CSharp {
+
+    public static class Messages {
+
+        public class StartCollecting {
+
+            public StartCollecting(int count) {
+                this.Count = count;
+            }
+
+            public int Count { get; set; }
+        }
+
+        public class StopCollecting {
+        }
+
+        public class Collected {
+
+            public Collected(string value) {
+                this.Value = value;
+            }
+
+            public string Value { get; set; }
+        }
+
+    }
+
+    public class ExampleActor : ReceiveActor {
+
+        private int _count = 0;
+        private readonly List<String> _values = new List<string>();
+
+        public ExampleActor() {
+            this.Waiting();
+        }
+
+        private void Waiting() {
+            this.Receive<Messages.StartCollecting>(message => {
+
+                Console.WriteLine("Starting collection");
+
+                _values.Clear();
+                _count = message.Count;
+
+                this.Become(this.Collecting);
+            });
+        }
+
+        private void Collecting() {
+            this.Receive<Messages.Collected>(message => {
+                
+                _values.Add(message.Value);
+
+                if(_values.Count() == _count) {
+
+                    Console.WriteLine(
+                        "Finished collecting ({0}/{0}): {1}", 
+                        _count, 
+                        String.Join(",", _values)
+                    );
+
+                    this.StopCollecting();
+                }
+            });
+
+            this.Receive<Messages.StopCollecting>(message => {
+
+                Console.WriteLine(
+                    "Stopped collecting ({0}/{1}): {2}", 
+                    _values.Count(), 
+                    _count, 
+                    String.Join(",", _values)
+                );
+
+                this.StopCollecting();
+            });
+        }
+
+        private void StopCollecting() {
+            this.Become(this.Waiting);
+
+            //Or stop
+            //ReceiveActor.Context.Stop(this.Self);
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            var system = ActorSystem.Create("ExampleSystem");
+            var actor = system.ActorOf<ExampleActor>("ExampleActor");
+
+            actor.Tell(new Messages.Collected("Hello, World (0)")); //Unhandled
+            actor.Tell(new Messages.StartCollecting(3));
+            actor.Tell(new Messages.Collected("Hello, World (1)"));
+            actor.Tell(new Messages.Collected("Hello, World (2)"));
+            actor.Tell(new Messages.Collected("Hello, World (3)"));
+            actor.Tell(new Messages.Collected("Hello, World (4)")); //Unhandled (or dead letter if actor stopped)
+
+            Console.ReadLine();
+
+            system.Shutdown();
+        }
+    }
+}
---
+++ CSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tribal")]
+[assembly: AssemblyProduct("CSharp")]
+[assembly: AssemblyCopyright("Copyright © Tribal 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57777f2b-e2b6-423e-9212-3fcc8b2f0648")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
