ec797e67076b320232f93bb9539219cb3c0058f3
Mark Watts <mark.watts@hotmail.co.uk>
2015-11-23 15:44:06 +0000

    Update AssemblyInfo.fs

--- FSharp/AssemblyInfo.fs
+++ FSharp/AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("AkkaStates")>]
+[<assembly: AssemblyTitle("FSharp")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("AkkaStates")>]
+[<assembly: AssemblyProduct("FSharp")>]
 [<assembly: AssemblyCopyright("Copyright ©  2015")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
